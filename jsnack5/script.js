b615b1f03e5e745a770879fd429635858aafe2ca
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-12 17:46:17 +0100

    refactor exercise 5

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,20 +1,21 @@
 let string = prompt("Inserisci una frase");
 
-
 function capitalize(fullPhrase) {
+
     fullPhrase = string.split(" ");
     let letterArrays = [];
     let finalString;
 
     for (let i = 0; i < fullPhrase.length; i++) {
-        let letter = fullPhrase[i].slice(0, 1).toUpperCase();
 
-        let sliced = letter + fullPhrase[i].slice(1);
-
+        let sliced = fullPhrase[i].slice(0, 1).toUpperCase() + fullPhrase[i].slice(1);
         letterArrays.push(sliced);
         finalString = letterArrays.join(" ");
+
     }
+
     return finalString;
+
 }
 
 document.querySelector("#result").innerHTML = capitalize(string);
